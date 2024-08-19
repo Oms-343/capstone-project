4796ff655db9429037e523ed412c7d58502efa0c
Oms-343 <oms87473@gmail.com>
2024-08-19 13:58:29 +0530

    responsive styles complete for nav header and main

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,55 +4,57 @@ function Main() {
       <h1>This week specials</h1>
       <button className="btn-cta-2">Online Menu</button>
 
-      <div className="food-card">
-        <img src="/assets/greek-salad.jpg" alt="" />
+      <div className="card-container">
+        <div className="food-card">
+          <img src="/assets/greek-salad.jpg" alt="" />
 
-        <div className="text-container">
-          <p className="labels">
-            <span>Greek salad</span>
-            <span>$12.99</span>
-          </p>
+          <div className="text-container">
+            <p className="labels">
+              <span>Greek salad</span>
+              <span>$12.99</span>
+            </p>
 
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
-            tempore necessitatibus impedit rem at consectetur.
-          </p>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+              tempore necessitatibus impedit rem at consectetur.
+            </p>
 
-          <button className="btn-card">Order Now</button>
+            <button className="btn-card">Order Now</button>
+          </div>
         </div>
-      </div>
-      <div className="food-card">
-        <img src="/assets/lemon-dessert.jpg" alt="" />
+        <div className="food-card">
+          <img src="/assets/lemon-dessert.jpg" alt="" />
 
-        <div className="text-container">
-          <p className="labels">
-            <span>Greek salad</span>
-            <span>$12.99</span>
-          </p>
+          <div className="text-container">
+            <p className="labels">
+              <span>Greek salad</span>
+              <span>$12.99</span>
+            </p>
 
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
-            tempore necessitatibus impedit rem at consectetur.
-          </p>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+              tempore necessitatibus impedit rem at consectetur.
+            </p>
 
-          <button className="btn-card">Order Now</button>
+            <button className="btn-card">Order Now</button>
+          </div>
         </div>
-      </div>
-      <div className="food-card">
-        <img src="/assets/bruchetta.svg" alt="" />
+        <div className="food-card">
+          <img src="/assets/bruchetta.svg" alt="" />
 
-        <div className="text-container">
-          <p className="labels">
-            <span>Greek salad</span>
-            <span>$12.99</span>
-          </p>
+          <div className="text-container">
+            <p className="labels">
+              <span>Greek salad</span>
+              <span>$12.99</span>
+            </p>
 
-          <p className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
-            tempore necessitatibus impedit rem at consectetur.
-          </p>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+              tempore necessitatibus impedit rem at consectetur.
+            </p>
 
-          <button className="btn-card">Order Now</button>
+            <button className="btn-card">Order Now</button>
+          </div>
         </div>
       </div>
     </main>
